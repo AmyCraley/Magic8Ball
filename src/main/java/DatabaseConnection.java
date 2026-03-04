206111bed5b22095a2a2ecfa115e29cdfd5e77bb
Amy Craley <amycraley18@gmail.com>
2026-03-03 19:36:48 -0500

    Still trying

--- src/main/java/DatabaseConnection.java
+++ src/main/java/DatabaseConnection.java
@@ -21,4 +21,19 @@ public class DatabaseConnection {
     }
 }
 
+//import java.sql.Connection;
+//import java.sql.DriverManager;
+//import java.sql.SQLException;
+//
+//public class DatabaseConnection {
+//
+//    private static final String URL = "jdbc:postgresql://localhost:5432/yourDatabaseName";
+//    private static final String USER = "postgres";
+//    private static final String PASSWORD = "yourPassword";
+//
+//    public static Connection connect() throws SQLException {
+//        return DriverManager.getConnection(URL, USER, PASSWORD);
+//    }
+//}
+
 
--- src/main/java/JdbcAnswersDao.java
+++ src/main/java/JdbcAnswersDao.java
@@ -15,15 +15,10 @@ import org.springframework.stereotype.Component;
 
 //import com.techelevator.model.User;
 
+
 @Component
-public class JdbcAnswersDao {
-
-    // Unsure what needs to go here
-}
-
-@Override
 public Answer getAnswerById(int category) {
     Answer answer = null;
     String sql = "SELECT * FROM magic8Ball WHERE category = ?"; //for user to choose category by number NO
-    //No need to return one random answer from the answer column with a category of 2, not the entire HashMap
+    //No, it needs to return one random answer from the answer column with a category of 2, not the entire HashMap
 }
