754951b356593327a3068c8434cbaefc4fc476c4
Amy Craley <amycraley18@gmail.com>
2026-04-19 11:19:10 -0400

    .

---
+++ src/main/java/com/amy/magic/Answer.java
@@ -0,0 +1,4 @@
+package com.amy.magic;
+
+public class Answer {
+}
--- src/main/java/com/amy/magic/JdbcAnswersDao.java
+++ src/main/java/com/amy/magic/JdbcAnswersDao.java
@@ -1,11 +1,12 @@
 package com.amy.magic;// package ?
 
-//import com.techelevator.exception.DaoException;
-//import com.techelevator.model.RegisterUserDto;
+
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.Random;
 
 @Component
 public class JdbcAnswersDao {
@@ -17,21 +18,30 @@ public class JdbcAnswersDao {
     }
 
     public Answer getRandomAnswerByCategory(int category) {
-        ArrayList<Answer> answers = null;
-        String sql = "SELECT * FROM magic8Ball WHERE category = ?"; //for user to choose category by number NO
+        ArrayList<Answer> answers = new ArrayList<>();
+        String sql = "SELECT * FROM magic8Ball WHERE category = ? ORDER BY RANDOM() LIMIT 1"; //for user to choose category by number
 
-        try {
-            SqlRowSet results = jdbcTemplate.queryForRowSet(sql);
-            while (results.next()) {
-                Answer answer = mapRowToUser(results);
-                answers.add(answer);
+
+            SqlRowSet results = jdbcTemplate.queryForRowSet(sql, category);
+            if (results.next()) {
+               return mapRowToAnser(results);
             }
-            return answers;
+            return null;
+
+        //pick a random answer
+        if (answers.isEmpty()){
+            return null;
         }
-        //No, it needs to return one random answer from the answer column with a category of 2, not the entire HashMap
+
+        Random random = new Random();
+        return answer.get(random.nextInt(answers.size()));
     }
-
-
+    private Answer mapRowToAnser(SqlRowSet rs) {
+        Answer answer = new Answer();
+        answer.setAnswer(rs.getString("answers"));
+        answer.setCategory(rs.getInt("category"));
+        return answer;
+    }
 }
 
 
